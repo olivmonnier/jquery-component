f28ed362c75f80d66f6c720fe5ddde701c705e03
olivmonnier <omonnier.dev@gmail.com>
2015-11-30 10:07:36 +0100

    add events on component

--- dist/jquery.component.js
+++ dist/jquery.component.js
@@ -1208,9 +1208,29 @@ module.exports = attempt;
 var template = require('lodash/string/template');
 
 (function($) {
-  $.component = function(){
+  $.fn.events = function(o){
+    for (var i in o) {
+      var eventTag = i.split(" ");
 
-  }
+      if (eventTag.length > 1) {
+        this.find(eventTag[1]).bind(i, o[i]);
+      } else {
+        this.bind(i, o[i]);
+      }
+
+    }
+    return this;
+  };
+
+  $.component = function(options){
+    return {
+      template: options.template || '',
+      events: options.events || {},
+      render: function(data) {
+        return $(template(this.template)(data)).events(this.events);
+      }
+    }
+  };
 }(jQuery));
 
 },{"lodash/string/template":31}]},{},[34]);
--- src/jquery.component.js
+++ src/jquery.component.js
@@ -1,7 +1,27 @@
 var template = require('lodash/string/template');
 
 (function($) {
-  $.component = function(){
+  $.fn.events = function(o){
+    for (var i in o) {
+      var eventTag = i.split(" ");
 
-  }
+      if (eventTag.length > 1) {
+        this.find(eventTag[1]).bind(i, o[i]);
+      } else {
+        this.bind(i, o[i]);
+      }
+
+    }
+    return this;
+  };
+
+  $.component = function(options){
+    return {
+      template: options.template || '',
+      events: options.events || {},
+      render: function(data) {
+        return $(template(this.template)(data)).events(this.events);
+      }
+    }
+  };
 }(jQuery));
