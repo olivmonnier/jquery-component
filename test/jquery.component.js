33e80b32b6fcfbceb35a8d6c92ca5f3fe831cc5b
olivmonnier <omonnier.dev@gmail.com>
2015-12-06 22:58:36 +0100

    improve some test

--- test/jquery.component.js
+++ test/jquery.component.js
@@ -3,8 +3,19 @@ QUnit.test('$.component', function(assert) {
   assert.equal(typeof $.component, 'function', 'Is function ?');
   assert.equal(typeof component, 'object', 'No parameters');
 });
-
+QUnit.test('$.component API bindData', function(assert) {
+  var component = $.component();
+  assert.equal(component.bindData, null, 'Is null');
+});
+QUnit.test('$.component API model', function(assert) {
+  var component = $.component();
+  assert.equal(typeof component.model, 'object', 'Is object');
+});
 QUnit.test('$.component API render', function(assert) {
   var component = $.component();
   assert.equal(typeof component.render, 'function', 'Is function ?');
+});
+QUnit.test('$.component API template', function(assert) {
+  var component = $.component();
+  assert.equal(typeof component.template, 'string', 'Is string');
 });
