946524ef5cee39d6090307082d13f73fd6655bbe
Olivier Monnier <omonnier.dev@gmail.com>
2016-06-02 12:12:46 +0200

    more test

--- test/jquery.component.js
+++ test/jquery.component.js
@@ -12,9 +12,57 @@ QUnit.test('$.component API bindData', function(assert) {
   assert.equal(component.bindData, null, 'Is null');
 });
 
+QUnit.test('$.component API children', function(assert) {
+  var component = $.component();
+  assert.equal(component.children, '', 'Is an empty string');
+
+  var component2 = $.component({
+    template: '<span></span>',
+    children: function() {
+      return document.createElement('span');
+    }
+  });
+  component2.render();
+  assert.equal(typeof component2.children, 'object', 'Is become an object after render running');
+
+  var child = $.component({
+    template: '<span>Foo</span>'
+  });
+  var component3 = $.component({
+    template: '<div><div data-children></div></div>',
+    children: child.render()
+  });
+  component3.render();
+  assert.equal(component3.$el.children().length, 1, 'Is populating by children after render running');
+
+  var component4 = $.component({
+    template: '<div><div data-child="foo"></div></div>',
+    children: {
+      foo: child.render()
+    }
+  });
+  component4.render();
+  assert.equal(component4.$el.find('[data-child="foo"]').children().length, 1, 'Is populating by children object key');
+});
+
+QUnit.test('$.component API clone', function(assert) {
+  var component1 = $.component();
+  assert.equal(typeof component1.clone, 'function', 'Is function');
+
+  var component2 = component1.clone();
+  assert.equal(typeof component2, 'object', 'Is an object');
+  assert.equal(typeof component2.render, 'function', 'Is a deep clone');
+
+  component1.render({foo: 'bar'});
+  component2.render({foo: 'baz'});
+  assert.equal(component1.model.data.foo, 'bar', 'Is data declare on render launch for component1');
+  assert.equal(component2.model.data.foo, 'baz', 'Is data declare on render launch for component2');
+});
+
 QUnit.test('$.component API componentDidMount', function(assert) {
   var component1 = $.component();
   assert.equal(component1.componentDidMount, null, 'Is null');
+
   var count = 0;
   var component2 = $.component({
     componentDidMount: function() {
@@ -30,6 +78,7 @@ QUnit.test('$.component API componentDidMount', function(assert) {
 QUnit.test('$.component API componentDidUpdate', function(assert) {
   var component1 = $.component();
   assert.equal(component1.componentDidUpdate, null, 'Is null');
+
   var count = 0;
   var component2 = $.component({
     componentDidUpdate: function() {
@@ -38,6 +87,7 @@ QUnit.test('$.component API componentDidUpdate', function(assert) {
   });
   for (var i = 0; i < 3; i++) {
     component2.render();
+
     if (i === 0) {
       assert.equal(count, 0, 'Expect not run in first time on render');
     }
@@ -48,6 +98,7 @@ QUnit.test('$.component API componentDidUpdate', function(assert) {
 QUnit.test('$.component API componentWillMount', function(assert) {
   var component1 = $.component();
   assert.equal(component1.componentWillMount, null, 'Is null');
+
   var count = 0;
   var component2 = $.component({
     componentWillMount: function() {
@@ -63,6 +114,7 @@ QUnit.test('$.component API componentWillMount', function(assert) {
 QUnit.test('$.component API componentWillUpdate', function(assert) {
   var component1 = $.component();
   assert.equal(component1.componentWillUpdate, null, 'Is null');
+
   var count = 0;
   var component2 = $.component({
     componentWillUpdate: function() {
@@ -91,6 +143,7 @@ QUnit.test('$.component API model.data', function(assert) {
 QUnit.test('$.component API model.get', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.model.get, 'function', 'Is function');
+
   var component2 = $.component({
     model: {
       msg: 'Hello World'
@@ -102,6 +155,7 @@ QUnit.test('$.component API model.get', function(assert) {
 QUnit.test('$.component API model.set', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.model.set, 'function', 'Is function');
+
   var component2 = $.component({
     model: {
       msg: 'Hello World'
@@ -114,6 +168,12 @@ QUnit.test('$.component API model.set', function(assert) {
 QUnit.test('$.component API render', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.render, 'function', 'Is function');
+  assert.equal(component1.mounted, false, 'Is not declared as mounted before render running');
+
+  component1.render({foo: 'bar'});
+  assert.equal(component1.mounted, true, 'Is declared as mounted after render running');
+  assert.equal(component1.model.data.foo, 'bar', 'Is updating the model');
+
   var template = '<h1>Hello World</h1>';
   var component2 = $.component({ template: template });
   assert.deepEqual(component2.render(), $(template), 'Expect return element defined in template');
@@ -122,6 +182,7 @@ QUnit.test('$.component API render', function(assert) {
 QUnit.test('$.component API template', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.template, 'string', 'Is string');
+
   var component2 = $.component({
     template: $('body').html()
   });
@@ -131,6 +192,7 @@ QUnit.test('$.component API template', function(assert) {
 QUnit.test('$.component API setBindData', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.setBindData, 'function', 'Is function');
+
   var component2 = $.component();
   var bindData = function(val) {
     return val + val;
@@ -142,6 +204,7 @@ QUnit.test('$.component API setBindData', function(assert) {
 QUnit.test('$.component API setChildren', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.setChildren, 'function', 'Is function');
+
   var component2 = $.component();
   var children = '<h1>Hello World !</h1>';
   component2.setChildren(children);
@@ -151,6 +214,7 @@ QUnit.test('$.component API setChildren', function(assert) {
 QUnit.test('$.component API setEvents', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.setEvents, 'function', 'Is function');
+
   var component2 = $.component();
   var events = {
     'click body': function() {
@@ -164,6 +228,7 @@ QUnit.test('$.component API setEvents', function(assert) {
 QUnit.test('$.component API setModel', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.setModel, 'function', 'Is function');
+
   var component2 = $.component({
     model: {
       msg: 'Hello World'
@@ -178,6 +243,7 @@ QUnit.test('$.component API setModel', function(assert) {
 QUnit.test('$.component API setTemplate', function(assert) {
   var component1 = $.component();
   assert.equal(typeof component1.setTemplate, 'function', 'Is function');
+
   var component2 = $.component();
   var template = '<h1>Hello World !</h1>';
   component2.setTemplate(template);
